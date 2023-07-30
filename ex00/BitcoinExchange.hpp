0cfa2da088aef83d84e2fd9c143f357983f7f9ff
TinyProbe <137adv167@gmail.com>
2023-07-30 17:41:48 +0900

    update

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,9 +6,18 @@
 /*   By: tkong <tkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 15:24:44 by tkong             #+#    #+#             */
-/*   Updated: 2023/07/30 15:24:55 by tkong            ###   ########.fr       */
+/*   Updated: 2023/07/30 16:26:27 by tkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
 
+class BitcoinExchange {
 
+public:
+
+};
+
+#endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,9 +6,23 @@
 /*   By: tkong <tkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 15:24:49 by tkong             #+#    #+#             */
-/*   Updated: 2023/07/30 15:24:55 by tkong            ###   ########.fr       */
+/*   Updated: 2023/07/30 16:48:48 by tkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "BitcoinExchange.hpp"
+#include <iostream>
+#include <fstream>
 
-
+int main(int ac, char **av) {
+	if (ac != 2) {
+		std::cout << "Need 1 argument" << '\n';
+		return 1;
+	}
+	std::fstream fs(av[1]);
+	if (!fs.is_open()) {
+		std::cout << "Failed open file" << '\n';
+		return 2;
+	}
+	
+}
