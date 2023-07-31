1cdccb5877184a679c8f10e8bc69c834392e7d82
TinyProbe <137adv167@gmail.com>
2023-07-31 10:22:45 +0900

    update

--- ex00/BitcoinExchange.hpp
+++ ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: tkong <tkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 15:24:44 by tkong             #+#    #+#             */
-/*   Updated: 2023/07/30 16:26:27 by tkong            ###   ########.fr       */
+/*   Updated: 2023/07/30 18:11:04 by tkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,16 @@
 
 class BitcoinExchange {
 
+private:
+	BitcoinExchange();
+	BitcoinExchange(const BitcoinExchange &rhs);
+	BitcoinExchange &operator=(const BitcoinExchange &rhs);
+
 public:
+	BitcoinExchange(const std::string &db);
+	virtual ~BitcoinExchange();
+
+	virtual void run(const std::string &input);
 
 };
 
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tkong <tkong@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/30 15:24:49 by tkong             #+#    #+#             */
-/*   Updated: 2023/07/30 16:48:48 by tkong            ###   ########.fr       */
+/*   Updated: 2023/07/30 18:04:13 by tkong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,14 @@
 #include <iostream>
 #include <fstream>
 
+#define DB "data.csv"
+
 int main(int ac, char **av) {
-	if (ac != 2) {
-		std::cout << "Need 1 argument" << '\n';
-		return 1;
+	try {
+		if (ac != 2) { throw std::runtime_error("Need 1 argument"); }
+	} catch (const std::exception &e) {
+		std::cout << e.what() << '\n';
 	}
-	std::fstream fs(av[1]);
-	if (!fs.is_open()) {
-		std::cout << "Failed open file" << '\n';
-		return 2;
-	}
-	
+	BitcoinExchange btex(DB);
+	btex.run(av[1]);
 }
